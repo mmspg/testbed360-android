1f8a9ec045cd0cebd757603ba608f20f4d9da55f
LM Garret <max.garret@wanadoo.fr>
2018-02-21 10:23:21 +0100

    Added copyright headers
    Removed some unused files

--- app/src/main/java/ch/epfl/mmspg/testbed360/image/ImagesSession.java
+++ app/src/main/java/ch/epfl/mmspg/testbed360/image/ImagesSession.java
@@ -1,3 +1,20 @@
+// Copyright (C) 2017 ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland
+// Multimedia Signal Processing Group
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+
 package ch.epfl.mmspg.testbed360.image;
 
 import android.app.Activity;
@@ -82,7 +99,7 @@ public class ImagesSession {
         //And we init the evaluation pictures
         vrImgs = ImageUtils.distinctShuffle(ImageUtils.loadVRImages(context, sessionDir, VRScene.MODE_EVALUATION));
         Collections.reverse(vrImgs); // we reverse here as it will be inverted in the stack after
-        evaluationImages.addAll(vrImgs);
+       evaluationImages.addAll(vrImgs);
 
         vrImgs.clear();
 
